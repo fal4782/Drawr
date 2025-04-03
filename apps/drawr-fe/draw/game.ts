6ca234655b83a1a248b15400edecda7f674e8701
fal4782 <falguni4782@gmail.com>
2025-04-04 02:28:08 +0530

    saveAsPng cleanup

--- apps/drawr-fe/draw/game.ts
+++ apps/drawr-fe/draw/game.ts
@@ -263,9 +263,6 @@ export class Game {
     this.clearCanvas();
   }
   exportAsPNG(): string {
-    this.ctx.save(); // Save current transformation state
-    this.ctx.setTransform(1, 0, 0, 1, 0, 0); // Reset transformations to render the full canvas
-
     // Create a temporary canvas with the same dimensions
     const tempCanvas = document.createElement("canvas");
     const tempCtx = tempCanvas.getContext("2d")!;
@@ -278,8 +275,6 @@ export class Game {
     tempCtx.fillRect(0, 0, tempCanvas.width, tempCanvas.height);
 
     tempCtx.drawImage(this.canvas, 0, 0); // Draw the current canvas content onto the temp canvas
-
-    this.ctx.restore(); // Restore the original transformation
 
     return tempCanvas.toDataURL("image/png");
   }
